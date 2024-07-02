6280a8aeb4304e0bb221fb04a3beda3d396164d8
Pedro Serrano Faria <pedroserrano2@gmail.com>
2024-07-02 19:04:37 -0300

    Adjust configuration for Heroku deployment

--- app.py
+++ app.py
@@ -191,5 +191,4 @@ def main():
 
 # Entry point of the application
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8501))
-    st.run(main(), host='0.0.0.0', port=port)
+    main()
